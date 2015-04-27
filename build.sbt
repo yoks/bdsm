42e52f39ad02cde382989f8428fe7f8e84be91e9
Ivan <ivan.anisimov@nordigy.ru>
2015-04-27 19:18:37 +0300

    Implemented AgentFSM

--- build.sbt
+++ build.sbt
@@ -7,8 +7,15 @@ val akkaVersion = "2.3.10"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+
+  "io.kamon" %% "kamon-core" % "0.3.5",
+  "io.kamon" %% "kamon-statsd" % "0.3.5",
+
   "com.typesafe.akka" %% "akka-testkit" % akkaVersion % "test",
   "org.scalatest" %% "scalatest" % "2.2.4" % "test")
 
+javaOptions in run ++= Seq("-javaagent:./lib/aspectjweaver-1.8.5.jar")
+
+enablePlugins(JavaAppPackaging)
 
 fork in run := true
--- src/main/scala/pw/anisimov/TelephonyServer.scala
+++ src/main/scala/pw/anisimov/TelephonyServer.scala
@@ -1,15 +1,18 @@
 package pw.anisimov
 
-import akka.actor.{Actor, Props}
+import akka.actor.{ActorLogging, Actor, Props}
 import AgentFSM.Call
 
 object TelephonyServer {
   def props() = Props[TelephonyServer]
 }
 
-class TelephonyServer extends Actor {
+class TelephonyServer extends Actor with ActorLogging {
   override def receive: Receive = {
     case Call(connection) =>
       context.actorOf(connection)
+    case  msg =>
+      log.error(s"Unhandled message ${msg.getClass}")
+      log.info("{} message", msg)
   }
 }
